04b2b32fe4f54ecabaf337b5c3e805d8203a6acc
Alessandro Di Matteo <aledimatteo00@gmail.com>
2024-12-05 19:19:59 +0100

    fixed Individual and removed in-file test

--- symgp/individual.py
+++ symgp/individual.py
@@ -9,16 +9,20 @@ class Node:
         self.children.append(child)
     def __str__(self):
         return "("+self.function.__name__ +", " +", ".join([str(child) for child in self.children]) + ")"
-    
+
+
 class Leaf(Node):
     def __init__(self, value=None):
         self.function = None
         self.children = []
         self.value = value
     def evaluate(self):
+        if self.value is None:
+            raise ValueError("Leaf value is not set")
         return self.value
     def __str__(self):
         return str(self.value)
+
 
 class IndividualTree:
     inputLeaves:list["Node"]
@@ -40,30 +44,4 @@ class IndividualTree:
     def __str__(self):
         return str(self.root)
     
-
-if __name__ == "__main__":
-
-    # Define functions
-    def add(x, y):
-        return x + y
-
-    def sub(x, y):
-        return x - y
-
-    def mul(x, y):
-        return x * y
-
-    # Create nodes
-    leaf1 = Leaf()
-    leaf2 = Leaf()
-    leaf3 = Leaf(5)
-    add_node = Node(add, [leaf1, leaf2])
-    root = Node(mul, [add_node, leaf3])
-
-    # Create individual tree
-    tree = IndividualTree(root, [leaf1, leaf2])
-
-    # Evaluate the tree
-    result = tree.evaluate([3,4])
-    print(f"Result of the tree evaluation: {result}")
-    print(f"LISP of Tree structure: {tree}")
+__all__ = ["Node", "Leaf", "IndividualTree"]
