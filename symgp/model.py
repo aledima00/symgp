98abb4ebfc421d47b9e118a4c8c2ad647f0a4ad5
Alessandro Di Matteo <aledimatteo00@gmail.com>
2024-12-05 19:22:21 +0100

    added Model class

---
+++ symgp/model.py
@@ -0,0 +1,13 @@
+from .operators import Operator as _op
+from .individual import Leaf
+
+class Model:
+    max_depth:int
+    operators:list[_op]
+    inputLeaves:list[Leaf]
+    def __init__(self,max_depth:int,operators:list[_op],inputLeaves:list[Leaf]):
+        self.max_depth = max_depth
+        self.operators = operators
+        self.inputs = inputLeaves
+
+__all__ = ["Model"]
