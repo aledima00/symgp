5efb34cd4d8dfcaebde739d9f6d1d737f921cc2b
Alessandro Di Matteo <aledimatteo00@gmail.com>
2024-12-07 12:14:21 +0100

    name fix

--- symgp/operators/oprules.py
+++ symgp/operators/oprules.py
@@ -11,11 +11,11 @@ class OpRules:
     output_type:TypeSpec # fixed output type spec
     input_fcheck:Callable[[List[TypeSpec]], bool] # input type check function
 
-    def __init__(self, arity:int, output_frule:Callable[[List[TypeSpec]], TypeSpec], input_types:List[TypeSpec]=None, output_types:TypeSpec=None, input_fcheck:Callable[[List[TypeSpec]], bool]=None):
+    def __init__(self, arity:int, output_frule:Callable[[List[TypeSpec]], TypeSpec], input_types:List[TypeSpec]=None, output_type:TypeSpec=None, input_fcheck:Callable[[List[TypeSpec]], bool]=None):
         self.arity = arity
         self.output_frule = output_frule
         self.input_types = input_types if input_types is not None else [TypeSpec() for _ in range(arity)]
-        self.output_types = output_types if output_types is not None else TypeSpec()
+        self.output_type = output_type if output_type is not None else TypeSpec()
         self.input_fcheck = input_fcheck if input_fcheck is not None else lambda *x: True
 
 __all__ = ["OpRules"]
