c10c1a10b09f22944692f7e801981ad5e750260c
Alessandro Di Matteo <aledimatteo00@gmail.com>
2024-12-07 13:24:41 +0100

    full name operators

--- symgp/__init__.py
+++ symgp/__init__.py
@@ -1,5 +1,5 @@
 from .individual import IndividualTree, Leaf, Node
 from .model import Model
-from . import operators as op
+from . import operators
 
-__all__ = ["IndividualTree", "Leaf", "Node", "Model", "op"]
+__all__ = ["IndividualTree", "Leaf", "Node", "Model", "operators"]
