b6678cf52ffe9807b0b4df5d175f5dff49aef9da
Alessandro Di Matteo <aledimatteo00@gmail.com>
2024-12-13 16:27:02 +0100

    fixed import

--- symgp/operators/__init__.py
+++ symgp/operators/__init__.py
@@ -1,7 +1,7 @@
 from .operator import Operator
 from .oprules import OpRules
-from .rules import OUTPUT_FRULES, INPUT_BINARY_FCHECKS
+from .rules import OUTPUT_COMPUTE_RULES, INPUT_BINARY_CHECKS
 from .specifications import ShapeTypes, DataTypes, Specs
 
-__all__=["Operator", "OpRules", "OUTPUT_FRULES", "INPUT_BINARY_FCHECKS", "ShapeTypes", "DataTypes", "Specs"]
+__all__=["Operator", "OpRules", "OUTPUT_COMPUTE_RULES", "INPUT_BINARY_CHECKS", "ShapeTypes", "DataTypes", "Specs"]
 
