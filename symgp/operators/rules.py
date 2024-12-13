23917727e74e95b86349afb068636934881afca4
Alessandro Di Matteo <aledimatteo00@gmail.com>
2024-12-13 17:02:23 +0100

    fixed specs names

--- symgp/operators/rules.py
+++ symgp/operators/rules.py
@@ -8,8 +8,8 @@ class OUTPUT_COMPUTE_RULES(_Enum):
     Enum to represent the possible output type rules for an operator.
     Basically a function that obtain th output spces from the input specs.
     """
-    INHERIT_TYPE = 1 # output has same type as input [NPARRAY or SCALAR]
-    INHERIT_DTYPE = 2 # output has same dtype as input [INT, FLOAT, BOOL, STR]
+    INHERIT_SHAPE_TYPE = 1 # output has same type as input [NPARRAY or SCALAR]
+    INHERIT_DATA_TYPE = 2 # output has same dtype as input [INT, FLOAT, BOOL, STR]
     INHERIT_SHAPE = 3 # output has same shape as input
     TRANSPOSE_SHAPE = 4 # output has the shape of the input transposed
     MATMUL_SHAPE = 5 # output has the shape of the matrix multiplication of the inputs
@@ -21,8 +21,8 @@ class INPUT_BINARY_CHECKS(_Enum):
     Basically bool checks on input specs.
     It is used for binary operators, but if there are also some scalar inputs, they should be at the end of the list.
     """
-    SAME_TYPE = 1
-    SAME_DTYPE = 2
+    SAME_SHAPE_TYPE = 1
+    SAME_DATA_TYPE = 2
     SAME_SHAPE = 3
     TRANSPOSED_SHAPE = 4
 
