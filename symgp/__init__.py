276b381b5b2fb92af34aaf6ee5a3acc689922158
Alessandro Di Matteo <aledimatteo00@gmail.com>
2024-12-14 16:04:16 +0100

    fixed init import

--- symgp/__init__.py
+++ symgp/__init__.py
@@ -1,8 +1,8 @@
 from .individual import IndividualTree, Leaf, Node
 from .model import Model
 from . import operators
-import colorama
-colorama.init()
+import colorama as _clr
+_clr.init()
 from .format import Formatted
 
 __all__ = ["IndividualTree", "Leaf", "Node", "Model", "operators", "Formatted"]
