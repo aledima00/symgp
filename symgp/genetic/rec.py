98d880cc74c1698aec77ea0f3ff8e2fe95ca06c7
Alessandro Di Matteo <aledimatteo00@gmail.com>
2025-02-08 17:33:21 +0100

    fixed recombination with deepcopy

--- symgp/genetic/rec.py
+++ symgp/genetic/rec.py
@@ -3,24 +3,19 @@ import numpy as np
 
 
 class SubEx:
-    """
-    Exchanges a subtree from it1 with a subtree from it2.
-    Args:
-        it1 (IndividualTree): The first individual tree.
-        it2 (IndividualTree): The second individual tree.
-    Returns:
-        IndividualTree: A new individual tree.
-    """
     def __init__(self,rng:np.random.Generator):
         self.name="Subtree Exchange Operator"
         self.rng = rng
     def __call__(self,it1:IndividualTree,it2:IndividualTree):
-        nodes1 = it1.subnodes(keep_leaves=False)
-        nodes2 = it2.subnodes(keep_leaves=False)
+        ret1 = it1.deepCopy()
+        ret2 = it2.deepCopy()
+        nodes1 = ret1.subnodes(keep_leaves=False,keep_root=True)
+        nodes2 = ret2.subnodes(keep_leaves=False,keep_root=True)
         p1 = self.rng.choice(nodes1)
         p2 = self.rng.choice(nodes2)
         idx1 = self.rng.integers(0,len(p1.children))
         idx2 = self.rng.integers(0,len(p2.children))
         p1.children[idx1],p2.children[idx2] = p2.children[idx2],p1.children[idx1]
+        return ret1,ret2
 
 __all__ = ["SubEx"]
